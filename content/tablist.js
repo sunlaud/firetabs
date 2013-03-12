80376200fb9857880817c110ee06d8fc252c2043
sunlaud <sunlaud@mail.ru>
2013-03-12 04:09:32 +0200

    code separated into namespace

--- content/tablist.js
+++ content/tablist.js
@@ -1,72 +1,73 @@
-var TabList = {
-	onLoad: function(e) {
-		// initialization code
-		this.initialized = true;
-	},
+if(!sunlaud) var sunlaud={};
 
-	onMenuItemCommand: function(e) {
-		var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+(function() {
+
+    const log = {
+        debug: function(msg) {
+            Application.console.log("extension tablist: " + msg);
+        },
+        error: function(msg) {
+            Components.utils.reportError("extension tablist: " + msg);
+        }
+    };
+
+    /* get localized string by name */
+    function getString(name) {
+        try {
+            return document.getElementById("stringBundleTablist").getString(name);
+        } catch (ex) {
+            log.error("failed to get translation for: " + name + ", details:\n" + ex);
+        }
+        // fall off: if no translation - use the label after last dot
+        var lastDotIndex = name.lastIndexOf(".");
+        return (lastDotIndex > 0) ? name.substr(lastDotIndex + 1) : name;
+    }
+
+
+    function showTablist() {
+        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
                    .getService(Components.interfaces.nsIWindowMediator);
-		var winEnumerator = wm.getEnumerator("navigator:browser");
+        var winEnumerator = wm.getEnumerator("navigator:browser");
 
 
-		var winList = [];
-		while(winEnumerator.hasMoreElements()) {
-			var tabList = [];
-			var win = winEnumerator.getNext();
-			var tabs = win.getBrowser().browsers;
-			for(var i=0; i<tabs.length; i++) {
-				var doc = tabs[i].contentWindow.document;
-				tabList.push({title: doc.title, url: doc.location.href});
-			}
-			winList.push(tabList);
-		}
-		var url = "chrome://tablist/content/tablist.html";
-		var win = window.open(url);
-		/* wait for window load, events onLoad can not be bind due to unknown reason */
-		var timerId = setInterval(function() {
-			if(win.document && win.document.body) {
-				clearInterval(timerId);
-				var embedScript = win.document.createElement('script');
-				embedScript.setAttribute("type","text/javascript");
-				embedScript.innerHTML = "var winList = " + JSON.stringify(winList);
-				win.document.getElementsByTagName("head")[0].appendChild(embedScript);
-				//~ win.buildWinTabList();
-			}
-		}, 100);
-		//alert("test: " + getString("tablist.test") + " - " + getString("tablist.test.notranslate"));
-
-
-
-	}
-
-
-
-};
-
-
-const Log = {
-	debug: function(msg) {
-		Application.console.log("extension tablist: " + msg);
-	},
-	error: function(msg) {
-		Components.utils.reportError("extension tablist: " + msg);
-	}
-}
-
-function getString(name)
-{
-    try
-    {
-        return document.getElementById("stringBundleTablist").getString(name);
+        var winList = [];
+        while(winEnumerator.hasMoreElements()) {
+            var tabList = [];
+            var win = winEnumerator.getNext();
+            var tabs = win.getBrowser().browsers;
+            for(var i=0; i<tabs.length; i++) {
+                var doc = tabs[i].contentWindow.document;
+                tabList.push({title: doc.title, url: doc.location.href});
+            }
+            winList.push(tabList);
+        }
+        var url = "chrome://tablist/content/tablist.html";
+        var win = window.open(url);
+        /* wait for window load, events onLoad can not be bind due to unknown reason */
+        var timerId = setInterval(function() {
+            if(win.document && win.document.body) {
+                clearInterval(timerId);
+                var embedScript = win.document.createElement('script');
+                embedScript.setAttribute("type","text/javascript");
+                embedScript.innerHTML = "var winList = " + JSON.stringify(winList);
+                win.document.getElementsByTagName("head")[0].appendChild(embedScript);
+                //~ win.buildWinTabList();
+            }
+        }, 100);
+        //alert("test: " + getString("tablist.test") + " - " + getString("tablist.test.notranslate"));
     }
-    catch (err)
-    {
-        Log.error("failed to get translation for: " + name + ", details:\n" + err);
-    }
-    // falloff - use only the label after last dot.
-    var lastDotIndex = name.lastIndexOf(".");
-    return (lastDotIndex > 0) ? name.substr(lastDotIndex + 1) : name;
-}
+
+
+
+    sunlaud.TabList = {
+        onLoad: function(e) {
+            this.initialized = true;
+        },
+        onMenuItemCommand: function(e) {
+            showTablist();
+        }
+    };
+
+})();
 
 
